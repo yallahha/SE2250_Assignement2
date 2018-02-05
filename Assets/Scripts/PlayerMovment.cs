7dea193733a9c589e7938ed992aa230486d4fd44
yallahha <yallahha@uwo.ca>
2018-02-05 13:36:41 -0500

    first commit

---
+++ Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour {
+  public new Rigidbody rigidbody;
+    public float speed; 
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float movex = Input.GetAxis("Horizontal");
+        float movey = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(movex, 0.0f, movey);
+        rigidbody.AddForce (movement * speed);
+    }
+}
